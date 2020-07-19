0d080e78168896f45f36b9454ff9d2fa9ee2da13
LambdAurora <aurora42lambda@gmail.com>
2020-07-19 11:07:02 +0200

    Change LICENSE as GPLv3 was invalid in the context of a Minecraft mod without a special clause.

--- src/main/java/me/lambdaurora/keystrokes/command/KeystrokesCommand.java
+++ src/main/java/me/lambdaurora/keystrokes/command/KeystrokesCommand.java
@@ -1,19 +1,10 @@
 /*
- * AuroraKeystrokes
- * Copyright (C) 2019  LambdAurora
+ * Copyright © 2020 LambdAurora <aurora42lambda@gmail.com>
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This file is part of AuroraKeystrokes.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Licensed under the MIT license. For more information,
+ * see the LICENSE file.
  */
 
 package me.lambdaurora.keystrokes.command;
